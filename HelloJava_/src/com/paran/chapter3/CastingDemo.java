9ba226b57a2d088cb937bc55b82e14809ebed1ed
j <j@j-PC>
2017-06-26 11:05:19 +0900

    edit

--- HelloJava_/src/com/paran/chapter3/CastingDemo.java
+++ HelloJava_/src/com/paran/chapter3/CastingDemo.java
@@ -2,7 +2,7 @@ package com.paran.chapter3;
 /*
  * 암시적/명시적 형변환(Implicit/Explicit)
  *  1. 암시적 형변환
- *   변수 형태를 '어떤 변수'로 바꿀 건지 명시하지 아니하고 연산자 또는 기타 요인에 의해 변수의 형태를 변환하는 경우
+ *   변수 형태를 '어떤 변수'로 바꿀 건지 명시하지 않고 연산자 또는 기타 요인에 의해 변수의 형태를 변환하는 경우
  *   int num = 1.1f + 1; => num = 2.1f;
  *  2. 명시적 형변환
  *   바꾸고자 하는 변수 형태를 정확하게 명시하여 특정 변수로 형태를 변환하는 경우
@@ -12,7 +12,7 @@ package com.paran.chapter3;
  *    < -------- Down Casting ---------
  * byte - short - int - long - float - double
  *    ----------- Up Casting --------->
-
+ *
  * Up Casting : 본래 표현 가능한 변수 형태보다 표현 범위가 더 넓은 변수로 형태를 변환
  * Down Casting : 본래 표현 가능한 변수 형태보다 표현 범위가 더 좁은 변수로 형태를 변환
  * (class inheritance에서도 동일한 용어 사용. 단, 의미는 다름)
